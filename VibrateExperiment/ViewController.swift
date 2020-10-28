1aa3076cdcc532a1629fe7f1f3e731125230f4c5
Griffen Morrison <griffenmorrison@yahoo.com>
2020-10-28 13:57:26 -0500

    Initial Commit

---
+++ VibrateExperiment/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  VibrateExperiment
+//
+//  Created by Griffen Morrison on 10/28/20.
+//  Copyright © 2020 Griffen Morrison. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
